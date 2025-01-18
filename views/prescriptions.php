108acc5adecfdc773a1645cfdf137f68757b5c19
Mart Mbithi <martdevelopers254@gmail.com>
2025-01-18 14:03:57 +0300

    Craft Manage Prescriptions

---
+++ views/prescriptions.php
@@ -0,0 +1,164 @@
+<?php
+/*
+ *   Crafted On Sat Jan 18 2025
+ *   From his finger tips, through his IDE to your deployment environment at full throttle with no bugs, loss of data,
+ *   fluctuations, signal interference, or doubt—it can only be
+ *   the legendary coding wizard, Martin Mbithi (martin@devlan.co.ke, www.martmbithi.github.io)
+ *   
+ *   www.devlan.co.ke
+ *   hello@devlan.co.ke
+ *
+ *
+ *   The Devlan Solutions LTD Super Duper User License Agreement
+ *   Copyright (c) 2022 Devlan Solutions LTD
+ *
+ *
+ *   1. LICENSE TO BE AWESOME
+ *   Congrats, you lucky human! Devlan Solutions LTD hereby bestows upon you the magical,
+ *   revocable, personal, non-exclusive, and totally non-transferable right to install this epic system
+ *   on not one, but TWO separate computers for your personal, non-commercial shenanigans.
+ *   Unless, of course, you've leveled up with a commercial license from Devlan Solutions LTD.
+ *   Sharing this software with others or letting them even peek at it? Nope, that's a big no-no.
+ *   And don't even think about putting this on a network or letting a crowd join the fun unless you
+ *   first scored a multi-user license from us. Sharing is caring, but rules are rules!
+ *
+ *   2. COPYRIGHT POWER-UP
+ *   This Software is the prized possession of Devlan Solutions LTD and is shielded by copyright law
+ *   and the forces of international copyright treaties. You better not try to hide or mess with
+ *   any of our awesome proprietary notices, labels, or marks. Respect the swag!
+ *
+ *
+ *   3. RESTRICTIONS, NO CHEAT CODES ALLOWED
+ *   You may not, and you shall not let anyone else:
+ *   (a) reverse engineer, decompile, decode, decrypt, disassemble, or do any sneaky stuff to
+ *   figure out the source code of this software;
+ *   (b) modify, remix, distribute, or create your own funky version of this masterpiece;
+ *   (c) copy (except for that one precious backup), distribute, show off in public, transmit, sell, rent,
+ *   lease, or otherwise exploit the Software like it's your own.
+ *
+ *
+ *   4. THE ENDGAME
+ *   This License lasts until one of us says 'Game Over'. You can call it quits anytime by
+ *   destroying the Software and all the copies you made (no hiding them under your bed).
+ *   If you break any of these sacred rules, this License self-destructs, and you must obliterate
+ *   every copy of the Software, no questions asked.
+ *
+ *
+ *   5. NO GUARANTEES, JUST PIXELS
+ *   DEVLAN SOLUTIONS LTD doesn’t guarantee this Software is flawless—it might have a few
+ *   quirks, but who doesn’t? DEVLAN SOLUTIONS LTD washes its hands of any other warranties,
+ *   implied or otherwise. That means no promises of perfect performance, marketability, or
+ *   non-infringement. Some places have different rules, so you might have extra rights, but don’t
+ *   count on us for backup if things go sideways. Use at your own risk, brave adventurer!
+ *
+ *
+ *   6. SEVERABILITY—KEEP THE GOOD STUFF
+ *   If any part of this License gets tossed out by a judge, don’t worry—the rest of the agreement
+ *   still stands like a boss. Just because one piece fails doesn’t mean the whole thing crumbles.
+ *
+ *
+ *   7. NO DAMAGE, NO DRAMA
+ *   Under no circumstances will Devlan Solutions LTD or its squad be held responsible for any wild,
+ *   indirect, or accidental chaos that might come from using this software—even if we warned you!
+ *   And if you ever think you’ve got a claim, the most you’re getting out of us is the license fee you
+ *   paid—if any. No drama, no big payouts, just pixels and code.
+ *
+ */
+
+session_start();
+require_once('../config/config.php');
+require_once('../config/checklogin.php');
+require_once('../config/codeGen.php');
+check_login();
+require_once('../partials/head.php');
+?>
+
+<body class="nk-body npc-invest bg-lighter ">
+    <div class="nk-app-root">
+        <!-- wrap @s -->
+        <div class="nk-wrap ">
+            <!-- main header @s -->
+            <?php require_once('../partials/pos_header.php'); ?>
+            <!-- main header @e -->
+            <!-- content @s -->
+            <div class="nk-content nk-content-lg nk-content-fluid">
+                <div class="container-xl wide-lg">
+                    <div class="nk-content-inner">
+                        <div class="nk-content-body">
+                            <div class="nk-block-head">
+                                <div class="nk-block-between-md g-3">
+                                    <div class="nk-block-head-content">
+                                        <div class="align-center flex-wrap pb-2 gx-4 gy-3">
+                                            <div>
+                                                <h4 class="nk-block-title fw-normal">Manage Patient Prescriptions</h4>
+                                                <p>
+                                                    This module allows you to register, update and delete patient prescriptions <br>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div><!-- .nk-block-head-content -->
+                                </div><!-- .nk-block-between -->
+                            </div><!-- .nk-block-head -->
+                            <div class="nk-block">
+                                <div class="card mb-3 col-md-12 border border-success">
+                                    <div class="card-body">
+                                        <table class="datatable-init table">
+                                            <thead>
+                                                <tr>
+                                                    <th>Patient</th>
+                                                    <th>Doctor</th>
+                                                    <th>Date</th>
+                                                    <th>Manage</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                $user_id = $_SESSION['user_id'];
+                                                $ret = "SELECT * FROM prescriptions p INNER JOIN users u ON u.user_id = p.pres_doctor_id ";
+                                                $stmt = $mysqli->prepare($ret);
+                                                $stmt->execute(); //ok
+                                                $res = $stmt->get_result();
+                                                while ($prescriptions = $res->fetch_object()) { ?>
+                                                    <tr>
+                                                        <td>
+                                                            Name: <?php echo $prescriptions->pres_patient_name; ?> <br>
+                                                            Email: <?php echo $prescriptions->pres_patient_email; ?> <br>
+                                                            Phone: <?php echo $prescriptions->pres_patient_phoneno; ?>
+                                                        </td>
+                                                        <td>
+                                                            Name: <?php echo $prescriptions->user_name; ?> <br>
+                                                            Email: <?php echo $prescriptions->user_email; ?> <br>
+                                                            Phone: <?php echo $prescriptions->user_phoneno; ?>
+                                                        </td>
+                                                        <td><?php echo date('d M Y g:ia', strtotime($prescriptions->pres_date)); ?></td>
+                                                        <td>
+                                                            <a data-toggle="modal" href="#view_<?php echo $prescriptions->pres_id; ?>" class="badge badge-dim badge-pill badge-outline-success"><em class="icon ni ni-eye"></em> View</a>
+                                                        </td>
+                                                    </tr>
+                                                <?php include('../helpers/modals/precriptions_modals.php');
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- .card -->
+                    </div><!-- .col -->
+                </div><!-- .row -->
+            </div><!-- .nk-block -->
+        </div>
+    </div>
+    <!-- content @e -->
+    <!-- footer @s -->
+    <?php require_once('../partials/pos_footer.php');; ?>
+    <!-- footer @e -->
+    </div>
+    <!-- wrap @e -->
+    </div>
+    <!-- app-root @e -->
+    <!-- JavaScript -->
+    <?php require_once('../partials/scripts.php'); ?>
+</body>
+
+</html>
